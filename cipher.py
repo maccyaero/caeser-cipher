c95a9d1685355f251a0acc480fbd22e7d1449a68
maccyaero <17176445+maccyaero@users.noreply.github.com>
2022-06-22 18:10:03 +1000

    Add files via upload
    Added user input to select whether to encode or decode

--- cipher.py
+++ cipher.py
@@ -3,39 +3,41 @@ from operator import ne
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-UserInput = input("Type in the word to encode \n")
-UserInput = list(UserInput.lower())
-print(UserInput)
+UserInput = input("Type encode to encrpyt, decode to decrypt\n").lower()
+text = input("Type in the word to encode \n")
+text = list(text.lower())
 cipher=int(input("Type in cypher number\n"))
     
 def encode ():
-    global UserInput
+    global text
     global cipher
-    for letter in UserInput:
+    for letter in text:
         index = alphabet.index(letter)
-        print(index)
-        userindex = UserInput.index(letter)
-        print(userindex)
+        # print(index)
+        userindex = text.index(letter)
+        # print(userindex)
         newindex = index+cipher
-        print(newindex)
+        # print(newindex)
         newletter = alphabet[newindex]
-        UserInput[userindex] = newletter
+        text[userindex] = newletter
 
 def decode():
-    global UserInput
+    global text
     global cipher
-    for letter in UserInput:
+    for letter in text:
         index = alphabet.index(letter)
-        print(index)
-        userindex = UserInput.index(letter)
+        # print(index)
+        userindex = text.index(letter)
         if(index>cipher):
             newindex = index-cipher
             newletter = alphabet[newindex]
         else:
             newindex = cipher-index
             newletter = alphabet[newindex]
-        UserInput[userindex] = newletter
+        text[userindex] = newletter
 
-#encode()
-decode()
-print(UserInput)
+if (UserInput=="encode"):
+    encode()
+if (UserInput=="decode"):
+    decode()
+print(text)
